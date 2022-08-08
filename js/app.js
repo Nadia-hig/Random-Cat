f7b655a2e2139590c7a7f590efd5956f23c8af22
Nadia-hig <nadia_hig@mail.ru>
2022-08-08 17:57:12 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,21 @@
+const button = document.querySelector('.btn');
+const image = document.querySelector('.img');
+const url = "http://aws.random.cat/meow";
+
+async function fetchHandler() {
+   try {
+      const responce = await fetch(url);
+      const data = await responce.json();
+      image.src = data.file;
+   } catch (error) {
+      console.log(error);
+   }
+}
+
+button.addEventListener("click", () => {
+   let isLoaded = image.complete;
+
+   if (isLoaded) {
+      fetchHandler();
+   }
+});
